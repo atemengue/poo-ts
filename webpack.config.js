670074bc42c0258e04f83bdf766ff7a0dfac79af
atemengue <moafembe@gmail.com>
2022-10-22 09:49:46 +0100

    add: webpackconfig tsconfig

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+/** @format */
+
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts'],
+  },
+  ouput: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};

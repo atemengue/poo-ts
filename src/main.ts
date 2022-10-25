0377c5f0eb560ca56e148000c32e59dbb48a0ed3
atemengue <moafembe@gmail.com>
2022-10-25 02:40:40 +0100

    intro-start

--- src/main.ts
+++ src/main.ts
@@ -1,56 +1,14 @@
 /** @format */
-import CompteCourant from './scripts/compte-courant';
-import Renderer from './scripts/renderer';
 
-// creation d'une instance d' un compte
-
-class Main {
-  // proprietes de la classe
-  public compteCourant: CompteCourant;
-
-  // constructeur
-  constructor() {
-    this.compteCourant = new CompteCourant('Antoine Junoir');
-    this.afficheCompte();
-  }
-
-  // Affiche les comptes
-  afficheCompte() {
-    const html = (document.querySelector('#appTemplate').innerHTML = `
-    <h2> Bienvenu UBA Bank!</h2><br /><h5>Le solde de votre compte:</h5><br />
-      Nom du client 1: ${this.compteCourant.nomClient}
+const html = `
+      <h2> Bienvenu UBA Bank!</h2><br /><br />
+        
+      <h5>Nom du client:</h5>
       <br />
-      Solde du client: ${this.compteCourant.solde.toFixed(2)}
-
-      <br /><br />
-      
+      <h5>Solde du client:</h5>
+  
+      <br/><br />
+        
       <input type="text" id="creditEtDebit" />&nbsp;&nbsp;
-      <button onclick="main.crediterDebiter(true)">Crediter</button>&nbsp;
-      <button onclick="main.crediterDebiter(false)">Debiter</button>&nbsp;
-      
-
-    `);
-
-    Renderer.render(html);
-  }
-  // debiter et crediter des comptes
-
-  crediterDebiter(credit: boolean) {
-    const input: HTMLInputElement = document.querySelector('#creditEtDebit');
-    const valeurInput = parseInt(input.value);
-
-    if (credit) {
-      this.compteCourant.crediter(valeurInput);
-    } else {
-      this.compteCourant.debiter(valeurInput);
-    }
-
-    this.afficheCompte();
-  }
-}
-
-Renderer.appTemplate = document.querySelector('#appTemplate');
-const main = new Main();
-
-// exposer l'API du main dans la page web
-(<any>window).main = main;
+      <button onclick="">Crediter</button>&nbsp;
+      <button onclick="">Debiter</button>&nbsp;`;
